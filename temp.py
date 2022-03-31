4733df787e7ffd4c59655fbb7cbd688248abfe0a
name <cybersteer@naver.com>
2022-03-31 22:56:14 +0900

    commit

--- temp.py
+++ temp.py
@@ -1,14 +1,34 @@
-import turtle
-t = turtle.Turtle()
-t.shape('turtle')
-t.up()
-t.goto(-100,-100)
-t.down()
-t.color('green')
+import random
+# for i in range(2,10):
+#     for j in range(1,10):
+#         print(f'{i} * {j} = {i*j}')
+#     print("-"*15)
 
-for i in range(4):
-    t.forward(100)
-    t.right(90)
+# for i in range(2,10,2):
+#     for j in range(1,10):
+#         print(f'{i} * {j} = {i*j}')
+#     print("-"*15)
 
+# count = 0
+# number = 0
+# answer = random.randint(1,100)
+#
+# while number != answer:
+#     number = int(input("숫자 입력 : "))
+#     count += 1
+#     if number > answer:
+#         print(f"{number}보다 낮습니다")
+#     elif number <answer:
+#         print(f"{number}보다 높습니다")
+#     print(f"남은 시도 횟수 : {10-count}")
+#     print()
+#     if count >9:
+#         break
+#
+# if answer == number:
+#     print(f"{count}번만에 정답!")
+# else:
+#     print(f"정답은 {answer}입니다")
 
-turtle.done()
+for i in range(10,0,1):
+    print(i)
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-a = '10'
-a = int(a)
-print(a*2)
+str = input("문자 입력 : ")
+
+for i in range(len(str)+1):
+    print(str[0:i])
+
+print()
+for i in range(len(str)):
+    for j in range(i):
+        print(" ", end="")
+    print(str[i])
