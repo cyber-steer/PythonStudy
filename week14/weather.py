8ad3d0538dc7e6cc1ad823b3fcd4278081a1a78a
name <cybersteer@naver.com>
2022-06-10 21:56:23 +0900

    week15

--- week14/weather.py
+++ week14/weather.py
@@ -5,7 +5,7 @@ import requests
 api_key = '4344b0c5d63fb946ca403d0675a37478'
 city_name = 'yangsan'
 weather_url = 'https://api.openweathermap.org/data/2.5/weather?&q='+city_name+'&appid='+api_key
-
+# https://api.openweathermap.org/data/2.5/weather?id=524901&lang=kr&appid=4344b0c5d63fb946ca403d0675a37478
 print(weather_url)
 
 response = requests.get(weather_url)
---
+++ week15/WeatherProject.py
@@ -0,0 +1,9 @@
+#configure
+from tkinter import *
+
+window = Tk()
+window.geometry('500x500')
+
+Label()
+
+window.mainloop()
